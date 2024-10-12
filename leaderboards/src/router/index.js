6adcd64bee1d4311e51a15fe210eb7ee180c5708
kernelcurry <michael.ian.curry@gmail.com>
2024-10-12 16:14:56 -0700

    add back log

--- leaderboards/src/router/index.js
+++ leaderboards/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+console.log(import.meta.env.BASE_URL);
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
